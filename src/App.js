86df3e5822d1941dd00cf35c034fbb03aac4327e
ajay kumar <asalwania@gmail.com>
2021-06-11 09:06:45 +0530

    files deleted

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
-import './App.css';
-import React from 'react';
-import Person from './Person';
+import React from 'react'
 
-class App extends React.Component {
-  state = {
-    persons: [
-      { name: 'ajay', age: '28' },
-      { name: 'alka', age: 29 },
-    ]
-  }
-  switchNameHandler = (data) => {
-    // console.log('clicked')
-    this.setState({
-      persons: [
-        { name: data, age: 29 },
-        { name: 'ajay', age: 28 },
-      ]
-    })
-  }
-  changeNameHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: event.target.value, age: 29 },
-        { name: 'ajay', age: 28 },
-      ]
-    })
-  }
-
-  render() {
-    return (
-      <>
-        <Person
-          name={this.state.persons[0].name}
-          change={this.changeNameHandler}
-        />
-        <Person
-          name={this.state.persons[1].name}
-        />
-        <button onClick={() => this.switchNameHandler('pawar')}
-        >Switch name</button>
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      Hello world
+    </div>
+  )
 }
 
-export default App;
+export default App
